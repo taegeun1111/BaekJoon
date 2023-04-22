d67602445c30672097be6578ea86aa7346da2336
taegeun <122959190+taegeun1111@users.noreply.github.com>
2023-04-22 20:50:38 +0900

    [Bronze I] Title: 단어 공부, Time: 272 ms, Memory: 20144 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //알파벳 : 26
+        int arr[] = new int[26];
+
+        String s = br.readLine();
+
+        for (int i = 0; i < s.length(); i++){
+
+            if ('A' <= s.charAt(i) && s.charAt(i) <= 'Z') { // 대문자 범위
+                arr[s.charAt(i) - 'A']++;	// 해당 인덱스의 값 1 증가
+            }
+
+            else {	// 소문자 범위
+                arr[s.charAt(i) - 'a']++;
+            }
+        }
+
+        int max = -1;
+        char ch = '?';
+
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                ch = (char) (i + 65); // 대문자로 출력해야하므로 65를 더해준다.
+            }
+            else if (arr[i] == max) {
+                ch = '?';
+            }
+        }
+
+        System.out.print(ch);
+    }
+}
+
