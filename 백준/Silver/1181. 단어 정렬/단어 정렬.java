6379cbd68e7a14efd68c53bde3f32ec357d213f1
taegeun <122959190+taegeun1111@users.noreply.github.com>
2023-04-24 16:52:35 +0900

    [Silver V] Title: 단어 정렬, Time: 944 ms, Memory: 66640 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        String[] arr = new String[N];
+
+        in.nextLine();    // 개행 버림
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = in.nextLine();
+        }
+
+        Arrays.sort(arr, (s1, s2) -> { //값이 음수면 그대로, 양수면 순서 바꿈
+            // 단어 길이가 같을 경우
+            if (s1.length() == s2.length()) {
+                return s1.compareTo(s2);  //모르겠다
+            }
+            // 그 외의 경우
+            else {
+                return s1.length() - s2.length();
+            }
+        });
+
+
+        System.out.println(arr[0]);
+
+        for (int i = 1; i < N; i++) {
+            // 중복되지 않는 단어만 출력
+            if (!arr[i].equals(arr[i - 1])) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
+
+
