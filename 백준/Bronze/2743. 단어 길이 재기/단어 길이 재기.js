5c8e3fc619f2a9cca960c4d84783e969afc2dc08
taegeun <122959190+taegeun1111@users.noreply.github.com>
2023-07-26 17:36:12 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 124 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+let input = fs.readFileSync('/dev/stdin').toString().trim();
+// let input = fs.readFileSync('./input.txt').toString();
+
+console.log(input.length);
+
+
