b9573e8a41fd48f8311a0ea7a8e79e934548f9fb
taegeun <122959190+taegeun1111@users.noreply.github.com>
2023-12-28 23:59:14 +0900

    [level unrated] Title: 배열 만들기 2, Time: 9.45 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
@@ -0,0 +1,10 @@
+function solution(l, r) {
+    var answer = [];
+      for (let i = l; i <= r; i++) {
+        if (/^[05]+$/.test(i)) {
+          answer.push(i);
+        }
+      }
+    
+    return answer.length === 0 ? [-1] : answer;
+}
