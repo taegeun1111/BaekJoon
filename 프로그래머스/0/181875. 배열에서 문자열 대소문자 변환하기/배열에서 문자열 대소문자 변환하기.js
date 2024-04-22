55d88ff3b4b1357fe985d6b3e35646eb7cbd4b15
taegeun <122959190+taegeun1111@users.noreply.github.com>
2024-04-22 21:48:50 +0900

    [level 0] Title: 배열에서 문자열 대소문자 변환하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.js
@@ -0,0 +1,9 @@
+function solution(strArr) {
+    return strArr.map((str, index) => {
+        if (index % 2 === 0) {
+            return str.toLowerCase();
+        } else {
+            return str.toUpperCase();
+        }
+    });
+}
